ce2a788bfafc8dc4366da6c53f6855d48ea90eae
Sean Bethel <tech@jmbdc.com>
2021-02-26 12:18:08 -0500

    - initial seeder - create migration - .nova configuration from editor (TODO: gitignore or not depending on team preference) - Squid model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
+     * Parse the data file.
+     * $path = path to data file relative to base_path
+     * default = data.json
+     *
+     * @return Array
+     */
+
+    private function import($path = "data.json"){
+        $file_contents = json_decode(file_get_contents(base_path($path)), true);
+        dd($file_contents);
+    }
+
+
+    /**
      * Seed the application's database.
      *
      * @return void
@@ -14,5 +28,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        //! Importing the data.json file as a seed source
+        //  I'm using json fields to simulate relations.
+        //  Also in models i'm using accessors to the same effect.
+        $this->import();
+
     }
 }
