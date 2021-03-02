30c6f63bdd8b3c3e8ea3ff8b21345ee4805b023e
Sean Bethel <tech@jmbdc.com>
2021-03-01 19:33:01 -0500

    add an artisan command to build app initially for testing

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('build-app', function () {
+    if(!File::exists(database_path('database.sqlite'))){
+        touch(database_path('database.sqlite'));
+        $this->info('The empty sqlite file has been created. You may now run:');
+        $this->line('php artisan migrate --seed');
+        $this->info('');
+    }
+    else $this->info('database.sqlite exists.');
+})->purpose('Build the app initially for testing');
+
